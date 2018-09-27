e9593f69c225f024c017c02f5246fbb268805c75
Guo Li <liguo@wustl.edu>
2018-09-26 19:50:28 -0500

    Added 34 Search for a range

---
+++ Leetcode/Leetcode/0034SearchForARange.cpp
@@ -0,0 +1,39 @@
+/*
+34 Search for a Range
+
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> searchRange(vector<int>& nums, int target) {
+		auto bounds = equal_range(nums.begin(), nums.end(), target);
+
+		if (bounds.first == bounds.second) {
+			return { -1, -1 };
+		}
+		else
+		{
+			return { bounds.first - nums.begin(), bounds.second - nums.begin() - 1 };
+		}
+	}
+};
